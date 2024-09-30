47a4675422b14f351b27e3c3a7ad97aa42858ff5
Lucas Pinheiro <pinheiro.lucash@gmail.com>
2024-09-30 20:47:51 -0300

    criação do html

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,60 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive } from 'vue';
+
+  const estado = reactive({
+    operacao: 'soma',
+    numeroA: 0,
+    numeroB: 0,
+    resultado: 0
+  })
+
+  const soma = () => {
+    return parseFloat(numeroA + numeroB)
+  }
+
+  const subtrair = () => {
+    return parseFloat(numeroA - numeroB)
+  }
+
+  const dividir = () => {
+    return parseFloat(numeroA + numeroB)
+  }
+
+  const multiplicar = () => {
+    return parseFloat(numeroA + numeroB)
+  }
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+  <div class="container">
+    <header>
+    <h1 class="p-5 mb-1 my-4 bg-info text-center">Calculadora</h1>
+    <p>Soma, subtração, divisão e multiplicação</p>
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="mt-5 mb-5 d-flex justify-content-around">
+    <div class="mb-3" @change="valorNovo">
+      <input @change="evento => estado.numeroA = evento.target.value" type="number" placeholder="Digite aqui o numero">
+    </div>
+    <div>
+        <select @change="evento => estado.operacao = evento.target.value">
+          <option value="soma">Somar</option>
+          <option value="subtrair">Subtrair</option>
+          <option value="dividir">Dividir</option>
+          <option value="multiplicar">Multiplicar</option>
+        </select>
+    </div>
+    <div>
+      <input @change="evento => estado.numeroB = evento.target.value" type="number" placeholder="Digite aqui o numero">
+    </div>
+  </div>
+  <div class="col-md-12">
+    <p @change="evento => estado.resultado = evento.target.value" class="fw-bold fs-6 text-center mt-5">
+      O Resultado é {{ estado.resultado }}
+    </p>
+  </div>
+  </div>
 </template>
-
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
